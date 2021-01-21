0987c639eae660fa8b4f6dc01b5ada5bc4fdd775
Kanstantsin <kostya.zvankovich@gmail.com>
2021-01-21 23:17:13 +0100

    Create init.el

---
+++ init.el
@@ -0,0 +1,68 @@
+;; Our stuff
+
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(use-package try
+	:ensure t)
+
+(use-package which-key
+	:ensure t 
+	:config
+	(which-key-mode))
+
+(global-linum-mode 1)
+
+(ido-mode 1)
+
+(setq ido-enable-flex-matching t)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(use-package zenburn-theme
+:ensure t
+:config (load-theme 'leuven  t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(tide zenburn-theme which-key use-package typescript-mode try)))
+
+;;; tide setup
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;;; whitespace setup
+(global-whitespace-mode 1)
